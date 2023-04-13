105248e50e9eba5e08333ca63c1203b7d1a7a34b
Haddad <ahmedabduelrahmen.haddad@esprit.tn>
2023-04-13 01:38:27 +0100

    Update

--- server.js
+++ server.js
@@ -49,12 +49,12 @@ app.use('/prestataire_dmd', require('./modules/prestataires_dmd/prestataire_dmd.
 app.use(errorHandler);
 
 // start server local
-// const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
-// app.listen(port, () => console.log('Server listening on port ' + port));
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+app.listen(port, () => console.log('Server listening on port ' + port));
 
 // start server
-console.log('Server listening on port 443');
-https.createServer(options, app).listen(443);
+// console.log('Server listening on port 443');
+// https.createServer(options, app).listen(443);
 
 // //LocalTunnel
 // (async () => {
